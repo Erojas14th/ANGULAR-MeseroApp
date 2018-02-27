60f3e352c9a8b00cfa8c78669360f2c036b8a5c3
Erojas <erojas14ths@gmail.com>
2018-02-27 01:23:46 -0500

    Envio , captura y navegacion con Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,18 @@ import { ConsultaComponent } from './consulta/consulta.component';
 import { ConsumoComponent } from './consumo/consumo.component';
 import { PlatoComponent } from './plato/plato.component';
 
+import { PlatoDetalleComponent } from './plato/plato-detalle/plato-detalle.component';
+import { PlatoEdicionComponent } from './plato/plato-edicion/plato-edicion.component';
+import { PlatoInicioComponent } from './plato/plato-inicio/plato-inicio.component';
+
 // Navegaciones : Url y sus componentes
 const appRoutes: Routes = [
-    { path: 'plato', component: PlatoComponent},
+    { path: 'plato', component: PlatoComponent, children: [
+        { path: '', component: PlatoInicioComponent},        
+        { path: 'nuevo', component: PlatoEdicionComponent}, 
+        { path: ':id', component: PlatoDetalleComponent}, 
+        { path: ':id/editar', component: PlatoEdicionComponent}
+    ]},
     { path: 'consumo', component: ConsumoComponent},
     { path: 'consulta', component: ConsultaComponent},
     { path: '', redirectTo: 'plato', pathMatch: 'full'}
--- src/app/plato/plato-detalle/plato-detalle.component.ts
+++ src/app/plato/plato-detalle/plato-detalle.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Plato } from '../../_model/plato';
+import { PlatoService } from '../../_service/plato.service';
+import { ActivatedRoute, Router, Params } from '@angular/router';
 
 @Component({
   selector: 'app-plato-detalle',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlatoDetalleComponent implements OnInit {
 
-  constructor() { }
+  plato: Plato;
+  id: number;
+
+  // Inyectamos el service
+  // Como recibe parametros , lo capturaremos
+  constructor(private platoService: PlatoService, 
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = params['id']; //es el que esta definido en el routing
+        console.log(this.id);
+
+        this.plato = this.platoService.getPlatos()[this.id];
+        console.log(this.plato);
+      }
+    )
   }
 
+  // A nuestra url route le añadira el editar
+  // this.route ahora es  localhost:4200/plato/id
+  // con navigate le indico que continue a otro hijo.
+  // se dirige  : localhost:4200/plato/id/editar
+  editarPlato(){
+    this.router.navigate(['editar'], { relativeTo: this.route });
+  }
 }
