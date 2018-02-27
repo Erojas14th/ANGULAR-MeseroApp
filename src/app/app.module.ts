98cced3febef09270734ad4c91eac122abf418ab
Erojas <erojas14ths@gmail.com>
2018-02-26 21:21:34 -0500

    Creacion de componentes y uso de BootStrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ConsultaComponent } from './consulta/consulta.component';
+import { ConsumoComponent } from './consumo/consumo.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { PlatoComponent } from './plato/plato.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConsultaComponent,
+    ConsumoComponent,
+    FooterComponent,
+    HeaderComponent,
+    PlatoComponent
   ],
   imports: [
     BrowserModule
